c8e959f5eba0cfe3631d504b83bf1b4b6ea40997
Martin Georgiev <martin.georgiev@gmail.com>
2015-11-11 12:12:02 +0000

    Don't play lazy and don't extend Jsonb

--- src/MartinGeorgiev/Doctrine/DBAL/Types/TextArray.php
+++ src/MartinGeorgiev/Doctrine/DBAL/Types/TextArray.php
@@ -2,26 +2,83 @@
 
 namespace MartinGeorgiev\Doctrine\DBAL\Types;
 
+use Doctrine\DBAL\DBALException;
 use Doctrine\DBAL\Platforms\AbstractPlatform;
 
 /**
  * Implementation of Postgres' text[] data type
  */
-class TextArray extends Jsonb
+class TextArray extends AbstractType
 {
-    use JsonTransformer;
-
     /**
      * @var string
      */
     const TYPE_NAME = 'text[]';
 
     /**
-     * {@inheritDoc}
+     * Converts a value from its PHP representation to its database representation of the type.
+     *
+     * @param mixed $value The value to convert.
+     * @param AbstractPlatform $platform The currently used database platform.
+     *
+     * @return string The database representation of the value.
+     *
+     * @throws DBALException
      */
     public function convertToDatabaseValue($value, AbstractPlatform $platform)
     {
-        $convertedValue = parent::convertToDatabaseValue($value, $platform);
-        return $convertedValue === '[]' ? '{}' : $convertedValue;
+        if (is_null($value)) {
+            return null;
+        }
+        $encodedValue = $this->transformToPostgresTextArray($value);
+        if ($encodedValue === false) {
+            throw new DBALException('Given value content cannot be encoded to valid json.');
+        }
+        return $encodedValue;
+    }
+
+    /**
+     * @param array $phpValue
+     * @return string
+     */
+    protected function transformToPostgresTextArray($phpValue)
+    {
+        if (!is_array($phpValue)) {
+            return false;
+        }
+        if (!$phpValue) {
+            return '{}';
+        }
+        return '{"' . join('","', $phpValue) . '"}';
+    }
+
+    /**
+     * Converts a value from its database representation to its PHP representation of this type.
+     *
+     * @param string $value The value to convert.
+     * @param AbstractPlatform $platform The currently used database platform.
+     *
+     * @return array The PHP representation of the value.
+     */
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        if ($value === null) {
+            return null;
+        }
+        $textArray = $this->transformFromPostgresTextArray($value);
+        return $textArray;
+    }
+
+    /**
+     * @param string $postgresValue
+     * @return array
+     */
+    protected function transformFromPostgresTextArray($postgresValue)
+    {
+        if ($postgresValue === '{}') {
+            return [];
+        }
+        $trimmedPostgresValue = mb_substr($postgresValue, 2, -2);
+        return explode('","', $trimmedPostgresValue);
     }
 }
