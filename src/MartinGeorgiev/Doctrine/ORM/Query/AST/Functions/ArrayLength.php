a0a7b0cb549fcd2c6e3f9e5b408c54aba6ef8c41
Martin Georgiev <martin.georgiev@gmail.com>
2017-03-01 15:06:09 +0000

    Add support for array_length

---
+++ src/MartinGeorgiev/Doctrine/ORM/Query/AST/Functions/ArrayLength.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace MartinGeorgiev\Doctrine\ORM\Query\AST\Functions;
+
+/**
+ * Implementation of PostgreSql ARRAY_LENGTH()
+ * @see http://www.postgresql.org/docs/9.4/static/functions-array.html
+ *
+ * @since 0.9
+ * @author Martin Georgiev <martin.georgiev@gmail.com>
+ */
+class ArrayLength extends AbstractFunction
+{
+    protected function customiseFunction()
+    {
+        $this->setFunctionPrototype('array_length(%s, %s)');
+        $this->addLiteralMapping('StringPrimary');
+        $this->addLiteralMapping('InputParameter');
+    }
+}
