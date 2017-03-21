1cb0a7df23a234a68f312a0af57e29d34c7c24ed
Martin Georgiev <martin.georgiev@gmail.com>
2017-03-21 12:22:47 +0000

    Add support for JSON_ARRAY_LENGTH, JSONB_INSERT, JSONB_SET, JSONB_STRIP_NULLS, JSON_EACH_TEXT, JSON_STRIP_NULLS

---
+++ src/MartinGeorgiev/Doctrine/ORM/Query/AST/Functions/JsonArrayLength.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace MartinGeorgiev\Doctrine\ORM\Query\AST\Functions;
+
+/**
+ * Implementation of PostgreSql JSON_ARRAY_LENGTH()
+ * @see https://www.postgresql.org/docs/9.4/static/functions-json.html
+ *
+ * @since 0.10
+ * @author Martin Georgiev <martin.georgiev@gmail.com>
+ */
+class JsonArrayLength extends AbstractFunction
+{
+    protected function customiseFunction()
+    {
+        $this->setFunctionPrototype('json_array_length(%s)');
+        $this->addLiteralMapping('StringPrimary');
+    }
+}
