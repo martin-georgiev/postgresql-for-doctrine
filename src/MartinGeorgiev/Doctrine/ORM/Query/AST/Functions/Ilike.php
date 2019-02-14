20ba37290c5b5899b3964966be86249cbb5962e5
Martin Georgiev <martin-georgiev@users.noreply.github.com>
2019-02-14 10:19:53 +0000

    Manually apply CS fixer rules

--- src/MartinGeorgiev/Doctrine/ORM/Query/AST/Functions/Ilike.php
+++ src/MartinGeorgiev/Doctrine/ORM/Query/AST/Functions/Ilike.php
@@ -5,9 +5,11 @@ declare(strict_types=1);
 namespace MartinGeorgiev\Doctrine\ORM\Query\AST\Functions;
 
 /**
- * Implementation of PostgreSql ILIKE()
+ * Implementation of PostgreSql ILIKE().
+ *
  * @see    https://www.postgresql.org/docs/9.3/functions-matching.html
  * @since  1.1
+ *
  * @author llaakkkk <lenakirichokv@gmail.com>
  */
 class Ilike extends BaseFunction
