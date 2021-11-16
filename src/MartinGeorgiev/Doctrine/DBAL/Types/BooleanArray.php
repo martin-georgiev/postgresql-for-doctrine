e86b58e21f3004e555406348dd119c5e3d4c69cb
Antonio J. García Lagar <aj@garcialagar.es>
2021-11-16 16:25:15 +0100

    Add BooleanArray type (#93)

---
+++ src/MartinGeorgiev/Doctrine/DBAL/Types/BooleanArray.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MartinGeorgiev\Doctrine\DBAL\Types;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+
+/**
+ * Implementation of PostgreSql BOOL[] data type.
+ *
+ * @see https://www.postgresql.org/docs/9.4/static/arrays.html
+ * @since 1.5.3
+ *
+ * @author Antonio J. García Lagar <aj@garcialagar.es>
+ */
+class BooleanArray extends BaseArray
+{
+    /**
+     * @var string
+     */
+    protected const TYPE_NAME = 'bool[]';
+
+    public function convertToDatabaseValue($phpArray, AbstractPlatform $platform): ?string
+    {
+        if (\is_array($phpArray)) {
+            $phpArray = $platform->convertBooleansToDatabaseValue($phpArray);
+        }
+
+        /*
+         * $phpArray type will be checked by parent class
+         * @phpstan-ignore-next-line
+         */
+        return parent::convertToDatabaseValue($phpArray, $platform);
+    }
+
+    public function convertToPHPValue($postgresArray, AbstractPlatform $platform): ?array
+    {
+        $phpArray = parent::convertToPHPValue($postgresArray, $platform);
+        if (\is_array($phpArray)) {
+            $phpArray = \array_map(static function ($value) use ($platform) { return $platform->convertFromBoolean($value); }, $phpArray);
+        }
+
+        return $phpArray;
+    }
+}
---
+++ tests/MartinGeorgiev/Doctrine/DBAL/Types/BooleanArrayTest.php
@@ -0,0 +1,90 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\MartinGeorgiev\Doctrine\DBAL\Types;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use MartinGeorgiev\Doctrine\DBAL\Types\BooleanArray;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+class BooleanArrayTest extends TestCase
+{
+    /**
+     * @var AbstractPlatform|MockObject
+     */
+    private $platform;
+
+    /**
+     * @var BooleanArray|MockObject
+     */
+    private $fixture;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->platform = $this->createMock(AbstractPlatform::class);
+
+        $this->fixture = $this->getMockBuilder(BooleanArray::class)
+            ->addMethods([])
+            ->disableOriginalConstructor()
+            ->getMock();
+    }
+
+    public function validTransformations(): array
+    {
+        return [
+            [
+                'phpValue' => null,
+                'postgresValue' => null,
+                'platformValue' => null,
+            ],
+            [
+                'phpValue' => [],
+                'postgresValue' => '{}',
+                'platformValue' => [],
+            ],
+            [
+                'phpValue' => [true, false, true],
+                'postgresValue' => '{1,0,1}',
+                'platformValue' => ['1', '0', '1'],
+            ],
+        ];
+    }
+
+    /**
+     * @test
+     */
+    public function has_name(): void
+    {
+        $this->assertEquals('bool[]', $this->fixture->getName());
+    }
+
+    /**
+     * @test
+     * @dataProvider validTransformations
+     */
+    public function can_transform_from_php_value(?array $phpValue, ?string $postgresValue, ?array $platformValue): void
+    {
+        $this->platform->method('convertBooleansToDatabaseValue')
+            ->with($phpValue)
+            ->willReturn($platformValue);
+
+        $this->assertEquals($postgresValue, $this->fixture->convertToDatabaseValue($phpValue, $this->platform));
+    }
+
+    /**
+     * @test
+     * @dataProvider validTransformations
+     */
+    public function can_transform_to_php_value(?array $phpValue, ?string $postgresValue): void
+    {
+        $this->platform->method('convertFromBoolean')
+            ->with($this->anything())
+            ->willReturn($this->returnCallback('boolval'));
+
+        $this->assertEquals($phpValue, $this->fixture->convertToPHPValue($postgresValue, $this->platform));
+    }
+}
