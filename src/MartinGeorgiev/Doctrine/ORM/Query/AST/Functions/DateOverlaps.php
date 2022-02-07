01ce25f6aa9d5115489ee418300ab0f541169759
gallyamow <gallyamow@gmail.com>
2022-02-07 21:25:17 +0300

    Update src/MartinGeorgiev/Doctrine/ORM/Query/AST/Functions/DateOverlaps.php
    Co-authored-by: Martin Georgiev <martin-georgiev@users.noreply.github.com>

--- src/MartinGeorgiev/Doctrine/ORM/Query/AST/Functions/DateOverlaps.php
+++ src/MartinGeorgiev/Doctrine/ORM/Query/AST/Functions/DateOverlaps.php
@@ -8,7 +8,7 @@ namespace MartinGeorgiev\Doctrine\ORM\Query\AST\Functions;
  * Implementation of PostgreSql check if left date interval overlaps with right interval.
  *
  * @see https://www.postgresql.org/docs/9.6/functions-datetime.html
- * @since 1.6.1
+ * @since 1.7.0
  *
  * @author Ramil Gallyamov <gallyamow@gmail.com>
  */
