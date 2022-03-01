b6387f1c7370ecb99989a453380cfa82e4116431
Ian Jenkins <ian.jenkins@boxuk.com>
2022-03-01 17:14:10 +0000

    Add support for regex operators.
    Adding support for operators listed here: https://www.postgresql.org/docs/9.3/functions-matching.html#FUNCTIONS-POSIX-REGEXP

---
+++ src/MartinGeorgiev/Doctrine/ORM/Query/AST/Functions/NotRegexp.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MartinGeorgiev\Doctrine\ORM\Query\AST\Functions;
+
+/**
+ * Implementation of PostgreSql case-sensitive not regular expression check (using !~).
+ *
+ * @see https://www.postgresql.org/docs/9.3/functions-matching.html#FUNCTIONS-POSIX-REGEXP
+ * @since 0.1
+ *
+ * @author Ian Jenkins <ian@jenko.me>
+ */
+class NotRegexp extends BaseFunction
+{
+    protected function customiseFunction(): void
+    {
+        $this->setFunctionPrototype('(%s !~ %s)');
+        $this->addNodeMapping('StringPrimary');
+        $this->addNodeMapping('StringPrimary');
+    }
+}
