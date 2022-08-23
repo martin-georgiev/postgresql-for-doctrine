56333e3fe544693daa53ce3beae46d0b173aa555
a.dmitryuk <a.dmitryuk@movavi.com>
2022-08-23 10:11:16 +0700

    phpdoc

--- src/MartinGeorgiev/Doctrine/ORM/Query/AST/Functions/ColumnToJsonb.php
+++ src/MartinGeorgiev/Doctrine/ORM/Query/AST/Functions/ColumnToJsonb.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace MartinGeorgiev\Doctrine\ORM\Query\AST\Functions;
 
 /**
- * Column implementation of Postgresql TO_JSONB().
+ * Column casting implementation of Postgresql TO_JSONB().
  *
  * @see https://www.postgresql.org/docs/9.6/static/functions-json.html
  * @since 1.8
