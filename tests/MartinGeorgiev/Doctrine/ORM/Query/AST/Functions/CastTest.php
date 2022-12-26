3eeabe63d65acb0840e9bf7f09694a117d83397f
Martin Georgiev <martin-georgiev@users.noreply.github.com>
2022-12-26 14:53:41 +0000

    Apply suggested changes from CR

--- tests/MartinGeorgiev/Doctrine/ORM/Query/AST/Functions/CastTest.php
+++ tests/MartinGeorgiev/Doctrine/ORM/Query/AST/Functions/CastTest.php
@@ -19,14 +19,18 @@ class CastTest extends TestCase
     protected function getExpectedSqlStatements(): array
     {
         return [
-            'SELECT cast(c0_.integer1 AS text) AS sclr_0 FROM ContainsIntegers c0_',
+            'SELECT cast(c0_.integer1 as text) AS sclr_0 FROM ContainsIntegers c0_',
+            'SELECT cast(c0_.integer1 as varchar(8)) AS sclr_0 FROM ContainsIntegers c0_',
+            'SELECT cast(c0_.integer1 as numeric(8, 2)) AS sclr_0 FROM ContainsIntegers c0_',
         ];
     }
 
     protected function getDqlStatements(): array
     {
         return [
-            \sprintf('SELECT CAST(e.integer1, \'text\') FROM %s e', ContainsIntegers::class),
+            \sprintf('SELECT CAST(e.integer1 AS text) FROM %s e', ContainsIntegers::class),
+            \sprintf('SELECT CAST(e.integer1 AS varchar(8)) FROM %s e', ContainsIntegers::class),
+            \sprintf('SELECT CAST(e.integer1 AS numeric(8, 2)) FROM %s e', ContainsIntegers::class),
         ];
     }
 }
