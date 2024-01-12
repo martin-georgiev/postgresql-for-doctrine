339a3cf4f7a5ac48991bb7e4b0df5f42b36ef44c
Alexander Dmitryuk <xakzona@bk.ru>
2024-01-12 16:02:03 +0600

    Add StartWith function (#162)
    Co-authored-by: a.dmitryuk <a.dmitryuk@movavi.com>
    Co-authored-by: Martin Georgiev <martin-georgiev@users.noreply.github.com>

---
+++ src/MartinGeorgiev/Doctrine/ORM/Query/AST/Functions/StartsWith.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MartinGeorgiev\Doctrine\ORM\Query\AST\Functions;
+
+class StartsWith extends BaseFunction
+{
+    protected function customiseFunction(): void
+    {
+        $this->setFunctionPrototype('(STARTS_WITH(%s, %s))');
+        $this->addNodeMapping('StringPrimary');
+        $this->addNodeMapping('StringPrimary');
+    }
+}
