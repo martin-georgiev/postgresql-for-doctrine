90ef4b014c0cf0be0e334c9d2e08857f7680baa1
Colin Doig <colindoig@gmail.com>
2024-04-05 19:14:18 +1300

    Add support for REGEXP_REPLACE() (#203)
    Co-authored-by: Colin Doig <colin@wialus.co.nz>
    Co-authored-by: Martin Georgiev <martin-georgiev@users.noreply.github.com>

---
+++ src/MartinGeorgiev/Doctrine/ORM/Query/AST/Functions/FlaggedRegexpReplace.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MartinGeorgiev\Doctrine\ORM\Query\AST\Functions;
+
+/**
+ * Implementation of PostgreSql REGEXP_REPLACE().
+ *
+ * @see https://www.postgresql.org/docs/15/functions-matching.html#FUNCTIONS-POSIX-REGEXP
+ * @since 2.5.0
+ *
+ * @author Colin Doig
+ */
+class FlaggedRegexpReplace extends BaseFunction
+{
+    protected function customiseFunction(): void
+    {
+        $this->setFunctionPrototype('regexp_replace(%s, %s, %s, %s)');
+        $this->addNodeMapping('StringPrimary');
+        $this->addNodeMapping('StringPrimary');
+        $this->addNodeMapping('StringPrimary');
+        $this->addNodeMapping('StringPrimary');
+    }
+}
