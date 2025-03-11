d36e66c7d07e68e4d9dcda5adbfaf7d6cb2c5f55
Martin Georgiev <martin.georgiev@gmail.com>
2025-03-11 23:33:18 +0000

    Improve tests

--- tests/MartinGeorgiev/Doctrine/ORM/Query/AST/Functions/ArrayAggTest.php
+++ tests/MartinGeorgiev/Doctrine/ORM/Query/AST/Functions/ArrayAggTest.php
@@ -21,13 +21,15 @@ class ArrayAggTest extends TestCase
         return [
             // Basic usage
             'SELECT array_agg(c0_.text1) AS sclr_0 FROM ContainsTexts c0_',
+            // With concatenation
+            'SELECT array_agg(c0_.text1 || c0_.text2) AS sclr_0 FROM ContainsTexts c0_',
             // With ORDER BY
             'SELECT array_agg(c0_.text1 ORDER BY c0_.text1 ASC) AS sclr_0 FROM ContainsTexts c0_',
             'SELECT array_agg(c0_.text1 ORDER BY c0_.text1 DESC) AS sclr_0 FROM ContainsTexts c0_',
+            // With concatenation and ORDER BY
+            'SELECT array_agg(c0_.text1 || c0_.text2 ORDER BY c0_.text1 ASC) AS sclr_0 FROM ContainsTexts c0_',
             // With multiple ORDER BY columns
             'SELECT array_agg(c0_.text1 ORDER BY c0_.text1 ASC, c0_.text2 DESC) AS sclr_0 FROM ContainsTexts c0_',
-            // With expression
-            'SELECT array_agg(c0_.text1 || c0_.text2) AS sclr_0 FROM ContainsTexts c0_',
         ];
     }
 
@@ -35,10 +37,11 @@ class ArrayAggTest extends TestCase
     {
         return [
             \sprintf('SELECT ARRAY_AGG(e.text1) FROM %s e', ContainsTexts::class),
+            \sprintf('SELECT ARRAY_AGG(CONCAT(e.text1, e.text2)) FROM %s e', ContainsTexts::class),
             \sprintf('SELECT ARRAY_AGG(e.text1 ORDER BY e.text1) FROM %s e', ContainsTexts::class),
             \sprintf('SELECT ARRAY_AGG(e.text1 ORDER BY e.text1 DESC) FROM %s e', ContainsTexts::class),
+            \sprintf('SELECT ARRAY_AGG(CONCAT(e.text1, e.text2) ORDER BY e.text1) FROM %s e', ContainsTexts::class),
             \sprintf('SELECT ARRAY_AGG(e.text1 ORDER BY e.text1 ASC, e.text2 DESC) FROM %s e', ContainsTexts::class),
-            \sprintf('SELECT ARRAY_AGG(CONCAT(e.text1, e.text2)) FROM %s e', ContainsTexts::class),
         ];
     }
 }
