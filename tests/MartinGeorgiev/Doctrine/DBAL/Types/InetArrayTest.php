bf9392acabeb5f0d58b3c2a16ec1da059978650b
Martin Georgiev <martin.georgiev@gmail.com>
2025-04-22 14:58:17 +0100

    chore: apply Rector changes

--- tests/MartinGeorgiev/Doctrine/DBAL/Types/InetArrayTest.php
+++ tests/MartinGeorgiev/Doctrine/DBAL/Types/InetArrayTest.php
@@ -15,7 +15,7 @@ class InetArrayTest extends TestCase
     /**
      * @var AbstractPlatform&MockObject
      */
-    private AbstractPlatform $platform;
+    private MockObject $platform;
 
     private InetArray $fixture;
 
