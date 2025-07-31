8d18b35ddbc4c9a059b5e6b4e22f12490b28c3f6
Martin Georgiev <martin-georgiev@users.noreply.github.com>
2025-07-31 11:36:13 +0300

    feat(#401): improve JSON field extraction by adding index support (#402)

--- src/MartinGeorgiev/Doctrine/ORM/Query/AST/Functions/JsonGetField.php
+++ src/MartinGeorgiev/Doctrine/ORM/Query/AST/Functions/JsonGetField.php
@@ -4,8 +4,18 @@ declare(strict_types=1);
 
 namespace MartinGeorgiev\Doctrine\ORM\Query\AST\Functions;
 
+use Doctrine\ORM\Query\Lexer;
+use Doctrine\ORM\Query\Parser;
+use Doctrine\ORM\Query\QueryException;
+use Doctrine\ORM\Query\TokenType;
+use MartinGeorgiev\Utils\DoctrineOrm;
+
 /**
  * Implementation of PostgreSQL json field retrieval, filtered by key (using ->).
+ *
+ * Supports both string keys for object property access and integer indices for array element access:
+ * - JSON_GET_FIELD(json_column, 'property_name') -> json_column->'property_name'
+ * - JSON_GET_FIELD(json_column, 0) -> json_column->0
  *
  * @see https://www.postgresql.org/docs/9.4/static/functions-json.html
  * @since 0.1
@@ -17,7 +27,24 @@ class JsonGetField extends BaseFunction
     protected function customizeFunction(): void
     {
         $this->setFunctionPrototype('(%s -> %s)');
-        $this->addNodeMapping('StringPrimary');
-        $this->addNodeMapping('StringPrimary');
+    }
+
+    protected function feedParserWithNodes(Parser $parser): void
+    {
+        $shouldUseLexer = DoctrineOrm::isPre219();
+
+        $nodeForJsonDocumentName = $parser->StringPrimary();
+        $parser->match($shouldUseLexer ? Lexer::T_COMMA : TokenType::T_COMMA);
+
+        // Second parameter can be either an index or a property name
+        try {
+            $nodeForJsonIndexOrPropertyName = $parser->ArithmeticPrimary();
+        } catch (QueryException) {
+            // If ArithmeticPrimary fails (e.g., when encountering a property name rather than an index), try StringPrimary
+            $nodeForJsonIndexOrPropertyName = $parser->StringPrimary();
+        }
+
+        /* @phpstan-ignore-next-line assign.propertyType */
+        $this->nodes = [$nodeForJsonDocumentName, $nodeForJsonIndexOrPropertyName];
     }
 }
---
+++ tests/Integration/MartinGeorgiev/Doctrine/ORM/Query/AST/Functions/JsonGetFieldTest.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Integration\MartinGeorgiev\Doctrine\ORM\Query\AST\Functions;
+
+use MartinGeorgiev\Doctrine\ORM\Query\AST\Functions\JsonGetField;
+
+class JsonGetFieldTest extends JsonTestCase
+{
+    protected function getStringFunctions(): array
+    {
+        return [
+            'JSON_GET_FIELD' => JsonGetField::class,
+        ];
+    }
+
+    public function test_json_get_field_with_property_name(): void
+    {
+        $dql = "SELECT JSON_GET_FIELD(t.object1, 'name') as result FROM Fixtures\\MartinGeorgiev\\Doctrine\\Entity\\ContainsJsons t WHERE t.id = 1";
+        $result = $this->executeDqlQuery($dql);
+        $this->assertSame('"John"', $result[0]['result']);
+    }
+
+    public function test_json_get_field_with_index(): void
+    {
+        $dql = "SELECT JSON_GET_FIELD(JSON_GET_FIELD(t.object1, 'tags'), 0) as result FROM Fixtures\\MartinGeorgiev\\Doctrine\\Entity\\ContainsJsons t WHERE t.id = 1";
+        $result = $this->executeDqlQuery($dql);
+        $this->assertSame('"developer"', $result[0]['result']);
+    }
+
+    public function test_json_get_field_nested_object_access(): void
+    {
+        $dql = "SELECT JSON_GET_FIELD(JSON_GET_FIELD(t.object1, 'address'), 'city') as result FROM Fixtures\\MartinGeorgiev\\Doctrine\\Entity\\ContainsJsons t WHERE t.id = 1";
+        $result = $this->executeDqlQuery($dql);
+        $this->assertSame('"New York"', $result[0]['result']);
+    }
+
+    public function test_json_get_field_with_empty_array(): void
+    {
+        $dql = "SELECT JSON_GET_FIELD(JSON_GET_FIELD(t.object1, 'tags'), 0) as result FROM Fixtures\\MartinGeorgiev\\Doctrine\\Entity\\ContainsJsons t WHERE t.id = 3";
+        $result = $this->executeDqlQuery($dql);
+        $this->assertNull($result[0]['result']);
+    }
+
+    public function test_json_get_field_with_nonexistent_index(): void
+    {
+        $dql = "SELECT JSON_GET_FIELD(JSON_GET_FIELD(t.object1, 'tags'), 10) as result FROM Fixtures\\MartinGeorgiev\\Doctrine\\Entity\\ContainsJsons t WHERE t.id = 1";
+        $result = $this->executeDqlQuery($dql);
+        $this->assertNull($result[0]['result']);
+    }
+
+    public function test_json_get_field_with_nonexistent_property_name(): void
+    {
+        $dql = "SELECT JSON_GET_FIELD(t.object1, 'nonexistent') as result FROM Fixtures\\MartinGeorgiev\\Doctrine\\Entity\\ContainsJsons t WHERE t.id = 1";
+        $result = $this->executeDqlQuery($dql);
+        $this->assertNull($result[0]['result']);
+    }
+}
