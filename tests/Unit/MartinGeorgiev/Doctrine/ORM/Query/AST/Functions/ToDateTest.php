0359e0c75157daf40f3cc327400469bd0d5510e3
Martin Georgiev <martin.georgiev@gmail.com>
2025-08-03 15:19:33 +0300

    no message

--- tests/Unit/MartinGeorgiev/Doctrine/ORM/Query/AST/Functions/ToDateTest.php
+++ tests/Unit/MartinGeorgiev/Doctrine/ORM/Query/AST/Functions/ToDateTest.php
@@ -21,14 +21,14 @@ class ToDateTest extends TestCase
     protected function getExpectedSqlStatements(): array
     {
         return [
-            "SELECT to_date(c0_.text1, 'DD Mon YYYY') AS sclr_0 FROM ContainsTexts c0_",
+            'converts text to date using format pattern' => "SELECT to_date(c0_.text1, 'DD Mon YYYY') AS sclr_0 FROM ContainsTexts c0_",
         ];
     }
 
     protected function getDqlStatements(): array
     {
         return [
-            \sprintf("SELECT TO_DATE(e.text1, 'DD Mon YYYY') FROM %s e", ContainsTexts::class),
+            'converts text to date using format pattern' => \sprintf("SELECT TO_DATE(e.text1, 'DD Mon YYYY') FROM %s e", ContainsTexts::class),
         ];
     }
 
