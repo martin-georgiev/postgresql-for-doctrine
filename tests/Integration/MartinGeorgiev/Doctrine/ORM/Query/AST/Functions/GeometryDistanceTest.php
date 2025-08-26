da0b210e17c6124e5a538e8dd43d80314d91d874
Martin Georgiev <martin.georgiev@gmail.com>
2025-08-26 11:36:02 +0300

    no message

--- tests/Integration/MartinGeorgiev/Doctrine/ORM/Query/AST/Functions/GeometryDistanceTest.php
+++ tests/Integration/MartinGeorgiev/Doctrine/ORM/Query/AST/Functions/GeometryDistanceTest.php
@@ -19,7 +19,6 @@ class GeometryDistanceTest extends SpatialOperatorTestCase
     #[Test]
     public function geometry_distance_returns_correct_distance_between_test_points(): void
     {
-        // Distance between POINT(0 0) and POINT(1 1) from test data
         $dql = 'SELECT GEOMETRY_DISTANCE(g.geometry1, g.geometry2) as distance
                 FROM Fixtures\MartinGeorgiev\Doctrine\Entity\ContainsGeometries g
                 WHERE g.id = 1';
@@ -34,37 +33,12 @@ class GeometryDistanceTest extends SpatialOperatorTestCase
     #[Test]
     public function geometry_distance_returns_zero_for_identical_geometries(): void
     {
-        // Identical geometries should have zero distance
         $dql = 'SELECT GEOMETRY_DISTANCE(g.geometry1, g.geometry1) as distance
                 FROM Fixtures\MartinGeorgiev\Doctrine\Entity\ContainsGeometries g
                 WHERE g.id = 1';
 
         $result = $this->executeDqlQuery($dql);
         $this->assertEquals(0, $result[0]['distance'], 'Distance between identical geometries should be 0');
-    }
-
-    #[Test]
-    public function geometry_distance_with_horizontal_points(): void
-    {
-        // Distance between POINT(0 0) and POINT(5 0) should be 5
-        $dql = "SELECT GEOMETRY_DISTANCE('POINT(0 0)', 'POINT(5 0)') as distance
-                FROM Fixtures\MartinGeorgiev\Doctrine\Entity\ContainsGeometries g
-                WHERE g.id = 1";
-
-        $result = $this->executeDqlQuery($dql);
-        $this->assertEquals(5.0, $result[0]['distance'], 'Horizontal distance should be 5');
-    }
-
-    #[Test]
-    public function geometry_distance_with_vertical_points(): void
-    {
-        // Distance between POINT(0 0) and POINT(0 3) should be 3
-        $dql = "SELECT GEOMETRY_DISTANCE('POINT(0 0)', 'POINT(0 3)') as distance
-                FROM Fixtures\MartinGeorgiev\Doctrine\Entity\ContainsGeometries g
-                WHERE g.id = 1";
-
-        $result = $this->executeDqlQuery($dql);
-        $this->assertEquals(3.0, $result[0]['distance'], 'Vertical distance should be 3');
     }
 
     #[Test]
