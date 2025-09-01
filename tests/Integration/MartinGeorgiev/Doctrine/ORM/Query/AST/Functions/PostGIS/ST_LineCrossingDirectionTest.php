ff26bfdb8a41d1028df641edf3e3eb212a5d84c8
Martin Georgiev <martin.georgiev@gmail.com>
2025-09-01 21:01:53 +0300

    no message

--- tests/Integration/MartinGeorgiev/Doctrine/ORM/Query/AST/Functions/PostGIS/ST_LineCrossingDirectionTest.php
+++ tests/Integration/MartinGeorgiev/Doctrine/ORM/Query/AST/Functions/PostGIS/ST_LineCrossingDirectionTest.php
@@ -17,14 +17,14 @@ class ST_LineCrossingDirectionTest extends SpatialOperatorTestCase
     }
 
     #[Test]
-    public function returns_crossing_direction_when_lines_cross(): void
+    public function returns_left_to_right_crossing_direction(): void
     {
         $dql = 'SELECT ST_LineCrossingDirection(\'LINESTRING(0 0, 2 2)\', \'LINESTRING(0 2, 2 0)\') as result
                 FROM Fixtures\MartinGeorgiev\Doctrine\Entity\ContainsGeometries g
                 WHERE g.id = 1';
 
         $result = $this->executeDqlQuery($dql);
-        $this->assertIsInt($result[0]['result']);
+        $this->assertEquals(1, $result[0]['result']);
     }
 
     #[Test]
@@ -39,6 +39,28 @@ class ST_LineCrossingDirectionTest extends SpatialOperatorTestCase
     }
 
     #[Test]
+    public function returns_right_to_left_crossing_direction(): void
+    {
+        $dql = 'SELECT ST_LineCrossingDirection(\'LINESTRING(2 0, 0 2)\', \'LINESTRING(0 0, 2 2)\') as result
+                FROM Fixtures\MartinGeorgiev\Doctrine\Entity\ContainsGeometries g
+                WHERE g.id = 1';
+
+        $result = $this->executeDqlQuery($dql);
+        $this->assertEquals(-1, $result[0]['result']);
+    }
+
+    #[Test]
+    public function returns_multiple_crossings_direction(): void
+    {
+        $dql = 'SELECT ST_LineCrossingDirection(\'LINESTRING(0 0, 4 4)\', \'LINESTRING(1 3, 3 1)\') as result
+                FROM Fixtures\MartinGeorgiev\Doctrine\Entity\ContainsGeometries g
+                WHERE g.id = 1';
+
+        $result = $this->executeDqlQuery($dql);
+        $this->assertEquals(2, $result[0]['result'], "Multiple crossings should return 2");
+    }
+
+    #[Test]
     public function returns_crossing_direction_in_where_clause(): void
     {
         $dql = 'SELECT ST_LineCrossingDirection(\'LINESTRING(0 0, 2 2)\', \'LINESTRING(0 2, 2 0)\') as result
@@ -46,6 +68,7 @@ class ST_LineCrossingDirectionTest extends SpatialOperatorTestCase
                 WHERE ST_LineCrossingDirection(\'LINESTRING(0 0, 2 2)\', \'LINESTRING(0 2, 2 0)\') > 0';
 
         $result = $this->executeDqlQuery($dql);
-        $this->assertNotEmpty($result);
+        $this->assertCount(1, $result);
+        $this->assertEquals(1, $result[0]['result'], "Should return only one row when the filters for crossing lines is applied");
     }
 }
