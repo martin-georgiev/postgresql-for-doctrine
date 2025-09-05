37a4a3f9b4383604c4b47cbcde7be6634cc6cd77
Pierre-Yves Landuré <pierre-yves@landure.fr>
2025-09-05 08:00:34 +0200

    fix: narrow catch clauses in convert methods and add docblock
    - catch InvalidLtreeException instead of InvalidArgumentException.
    - add docblock with reference to PostgreSQL's Ltree documentation

--- src/MartinGeorgiev/Doctrine/DBAL/Types/Ltree.php
+++ src/MartinGeorgiev/Doctrine/DBAL/Types/Ltree.php
@@ -7,8 +7,17 @@ namespace MartinGeorgiev\Doctrine\DBAL\Types;
 use Doctrine\DBAL\Platforms\AbstractPlatform;
 use MartinGeorgiev\Doctrine\DBAL\Types\Exceptions\InvalidLtreeForDatabaseException;
 use MartinGeorgiev\Doctrine\DBAL\Types\Exceptions\InvalidLtreeForPHPException;
+use MartinGeorgiev\Doctrine\DBAL\Types\ValueObject\Exceptions\InvalidLtreeException;
 use MartinGeorgiev\Doctrine\DBAL\Types\ValueObject\Ltree as LtreeValueObject;
 
+/**
+ * Implementation of PostgreSQL LTREE data type.
+ *
+ * @see https://www.postgresql.org/docs/13/ltree.html
+ * @since 3.5
+ *
+ * @author Pierre-Yves Landuré
+ */
 final class Ltree extends BaseType
 {
     protected const TYPE_NAME = 'ltree';
@@ -23,7 +32,7 @@ final class Ltree extends BaseType
         if (\is_string($value)) {
             try {
                 return LtreeValueObject::fromString($value);
-            } catch (\InvalidArgumentException) {
+            } catch (InvalidLtreeException) {
                 throw InvalidLtreeForDatabaseException::forInvalidFormat($value);
             }
         }
@@ -41,7 +50,7 @@ final class Ltree extends BaseType
         if (\is_string($value)) {
             try {
                 $value = LtreeValueObject::fromString($value);
-            } catch (\InvalidArgumentException) {
+            } catch (InvalidLtreeException) {
                 throw InvalidLtreeForPHPException::forInvalidFormat($value);
             }
         }
