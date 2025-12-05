34a1f4022bb6136fbec734b283a7d1a031145e28
Thomas Landauer <thomas@landauer.at>
2025-12-06 00:04:17 +0100

    Finishing the Types class

--- src/MartinGeorgiev/Doctrine/DBAL/Type.php
+++ src/MartinGeorgiev/Doctrine/DBAL/Type.php
@@ -4,7 +4,39 @@ declare(strict_types=1);
 
 namespace MartinGeorgiev\Doctrine\DBAL;
 
-enum Type
+final class Type
 {
+    // Array Types
     public const BIGINT_ARRAY = 'bigint[]';
+    public const BOOLEAN_ARRAY = 'bool[]';
+    public const CIDR_ARRAY = 'cidr[]';
+    public const DOUBLE_PRECISION_ARRAY = 'double precision[]';
+    public const GEOGRAPHY_ARRAY = 'geography[]';
+    public const GEOMETRY_ARRAY = 'geometry[]';
+    public const INET_ARRAY = 'inet[]';
+    public const INTEGER_ARRAY = 'integer[]';
+    public const JSONB_ARRAY = 'jsonb[]';
+    public const MACADDR_ARRAY = 'macaddr[]';
+    public const POINT_ARRAY = 'point[]';
+    public const REAL_ARRAY = 'real[]';
+    public const SMALLINT_ARRAY = 'smallint[]';
+    public const TEXT_ARRAY = 'text[]';
+
+    // Scalar Types
+    public const CIDR = 'cidr';
+    public const GEOGRAPHY = 'geography';
+    public const GEOMETRY = 'geometry';
+    public const INET = 'inet';
+    public const JSONB = 'jsonb';
+    public const LTREE = 'ltree';
+    public const MACADDR = 'macaddr';
+    public const POINT = 'point';
+
+    // Range Types
+    public const DATERANGE = 'daterange';
+    public const INT4RANGE = 'int4range';
+    public const INT8RANGE = 'int8range';
+    public const NUMRANGE = 'numrange';
+    public const TSRANGE = 'tsrange';
+    public const TSTZRANGE = 'tstzrange';
 }
