47a163a65b03e489b432291268e03d5dceddac72
Martin Georgiev <martin.georgiev@gmail.com>
2026-02-09 19:24:22 +0000

    no message

--- src/MartinGeorgiev/Doctrine/ORM/Query/AST/Functions/SimilarTo.php
+++ src/MartinGeorgiev/Doctrine/ORM/Query/AST/Functions/SimilarTo.php
@@ -5,11 +5,17 @@ declare(strict_types=1);
 namespace MartinGeorgiev\Doctrine\ORM\Query\AST\Functions;
 
 /**
- * Implementation of PostgreSQL SIMILAR TO.
+ * Implementation of PostgreSQL SIMILAR TO operator.
+ *
+ * Performs SQL pattern matching.
  *
  * @see https://www.postgresql.org/docs/9.6/functions-matching.html
+ * @since 1.3
  *
  * @author Igor Lazarev <strider2038@yandex.ru>
+ *
+ * @example Using it in DQL with boolean comparison: "WHERE SIMILAR_TO(e.text, 'pattern') = TRUE"
+ * Returns boolean, must be used with "= TRUE" or "= FALSE" when used in WHERE clause in DQL.
  */
 class SimilarTo extends BaseFunction
 {
