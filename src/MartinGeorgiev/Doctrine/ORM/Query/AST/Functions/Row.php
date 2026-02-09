421a2bdc496cf68d66bf773fe257f6d82cc471d0
Martin Georgiev <martin.georgiev@gmail.com>
2026-02-09 19:31:44 +0000

    no message

--- src/MartinGeorgiev/Doctrine/ORM/Query/AST/Functions/Row.php
+++ src/MartinGeorgiev/Doctrine/ORM/Query/AST/Functions/Row.php
@@ -5,9 +5,16 @@ declare(strict_types=1);
 namespace MartinGeorgiev\Doctrine\ORM\Query\AST\Functions;
 
 /**
- * Implementation of PostgreSQL Row Constructor expression.
+ * Implementation of PostgreSQL ROW() constructor expression.
+ *
+ * Constructs a row value from arguments.
  *
  * @see https://www.postgresql.org/docs/14/sql-expressions.html#SQL-SYNTAX-ROW-CONSTRUCTORS
+ * @since 2.8
+ *
+ * @author Bruno Zanchettin
+ *
+ * @example Using it in DQL: "SELECT ROW(e.field1, e.field2) FROM Entity e"
  */
 class Row extends BaseVariadicFunction
 {
