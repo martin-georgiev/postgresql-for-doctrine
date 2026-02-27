cdd94a61c3d5734be8eedfe0926e43bcd4a9189a
Martin Georgiev <martin.georgiev@gmail.com>
2026-02-27 17:54:02 +0000

    no message

---
+++ tests/Integration/MartinGeorgiev/Doctrine/ORM/Query/AST/Functions/StripTest.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Integration\MartinGeorgiev\Doctrine\ORM\Query\AST\Functions;
+
+use MartinGeorgiev\Doctrine\ORM\Query\AST\Functions\Strip;
+use MartinGeorgiev\Doctrine\ORM\Query\AST\Functions\ToTsvector;
+use PHPUnit\Framework\Attributes\Test;
+
+class StripTest extends TextTestCase
+{
+    protected function getStringFunctions(): array
+    {
+        return [
+            'TO_TSVECTOR' => ToTsvector::class,
+            'STRIP' => Strip::class,
+        ];
+    }
+
+    #[Test]
+    public function can_strip_positions_from_tsvector(): void
+    {
+        $dql = 'SELECT STRIP(TO_TSVECTOR(t.text1)) as result FROM Fixtures\\MartinGeorgiev\\Doctrine\\Entity\\ContainsTexts t WHERE t.id = 2';
+        $result = $this->executeDqlQuery($dql);
+        $this->assertSame("'dolor' 'ipsum' 'lorem'", $result[0]['result']);
+    }
+
+    #[Test]
+    public function stripped_tsvector_differs_from_original(): void
+    {
+        $dql = 'SELECT TO_TSVECTOR(t.text1) as original, STRIP(TO_TSVECTOR(t.text1)) as result FROM Fixtures\\MartinGeorgiev\\Doctrine\\Entity\\ContainsTexts t WHERE t.id = 2';
+        $stripped = $this->executeDqlQuery($dql);
+
+        $dql = 'SELECT FROM Fixtures\\MartinGeorgiev\\Doctrine\\Entity\\ContainsTexts t WHERE t.id = 2';
+        $original = $this->executeDqlQuery($dql);
+
+        $this->assertSame("'dolor' 'ipsum' 'lorem'", $stripped[0]['result']);
+        $this->assertNotSame($original[0]['original'], $original[0]['result']);
+    }
+
+    #[Test]
+    public function can_strip_positions_from_literal_tsvector(): void
+    {
+        $dql = "SELECT STRIP(TO_TSVECTOR('lorem ipsum dolor')) as result FROM Fixtures\\MartinGeorgiev\\Doctrine\\Entity\\ContainsTexts t WHERE t.id = 1";
+        $result = $this->executeDqlQuery($dql);
+        $this->assertSame("'dolor' 'ipsum' 'lorem'", $result[0]['result']);
+    }
+}
