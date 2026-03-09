b51facec762cd82947646681dfb4b68d26528c40
Martin Georgiev <martin.georgiev@gmail.com>
2026-03-09 16:38:27 +0000

    no message

--- fixtures/MartinGeorgiev/Doctrine/Entity/ContainsVectors.php
+++ fixtures/MartinGeorgiev/Doctrine/Entity/ContainsVectors.php
@@ -5,13 +5,14 @@ declare(strict_types=1);
 namespace Fixtures\MartinGeorgiev\Doctrine\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use MartinGeorgiev\Doctrine\DBAL\Type;
 
 #[ORM\Entity()]
 class ContainsVectors extends Entity
 {
-    #[ORM\Column(type: 'vector')]
+    #[ORM\Column(type: Type::VECTOR)]
     public array $vector1;
 
-    #[ORM\Column(type: 'vector')]
+    #[ORM\Column(type: Type::VECTOR)]
     public array $vector2;
 }
