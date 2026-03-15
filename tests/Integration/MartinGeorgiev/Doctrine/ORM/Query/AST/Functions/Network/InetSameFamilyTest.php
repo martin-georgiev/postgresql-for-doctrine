5c09642c02ee0677a090b4920d876d8582afe0e3
Martin Georgiev <martin.georgiev@gmail.com>
2026-03-15 12:24:08 +0200

    no message

--- tests/Integration/MartinGeorgiev/Doctrine/ORM/Query/AST/Functions/Network/InetSameFamilyTest.php
+++ tests/Integration/MartinGeorgiev/Doctrine/ORM/Query/AST/Functions/Network/InetSameFamilyTest.php
@@ -17,22 +17,22 @@ class InetSameFamilyTest extends TestCase
     }
 
     #[Test]
-    public function returns_true_for_same_family_from_literals(): void
-    {
-        $dql = "SELECT INET_SAME_FAMILY('192.168.1.5', '10.0.0.1') as result
-                FROM Fixtures\\MartinGeorgiev\\Doctrine\\Entity\\ContainsNetworks t
-                WHERE t.id = 1";
-        $result = $this->executeDqlQuery($dql);
-        $this->assertTrue((bool) $result[0]['result']);
-    }
-
-    #[Test]
     public function returns_true_for_same_family_from_fields(): void
     {
         $dql = 'SELECT INET_SAME_FAMILY(t.ip, t.ip) as result
                 FROM Fixtures\\MartinGeorgiev\\Doctrine\\Entity\\ContainsNetworks t
                 WHERE t.id = 1';
         $result = $this->executeDqlQuery($dql);
-        $this->assertTrue((bool) $result[0]['result']);
+        $this->assertTrue($result[0]['result']);
+    }
+
+    #[Test]
+    public function returns_false_for_different_families_from_literals(): void
+    {
+        $dql = "SELECT INET_SAME_FAMILY('192.168.1.5', '2001:db8::1') as result
+                FROM Fixtures\\MartinGeorgiev\\Doctrine\\Entity\\ContainsNetworks t
+                WHERE t.id = 1";
+        $result = $this->executeDqlQuery($dql);
+        $this->assertFalse($result[0]['result']);
     }
 }
