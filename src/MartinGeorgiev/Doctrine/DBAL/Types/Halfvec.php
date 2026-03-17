0c9e6d8dcebc186080804a45e2528252fe304578
Martin Georgiev <martin.georgiev@gmail.com>
2026-03-17 17:31:18 +0200

    no message

--- src/MartinGeorgiev/Doctrine/DBAL/Types/Halfvec.php
+++ src/MartinGeorgiev/Doctrine/DBAL/Types/Halfvec.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace MartinGeorgiev\Doctrine\DBAL\Types;
 
-use Doctrine\DBAL\Platforms\AbstractPlatform;
 use MartinGeorgiev\Doctrine\DBAL\Type;
 use MartinGeorgiev\Doctrine\DBAL\Types\Exceptions\InvalidHalfvecForDatabaseException;
 use MartinGeorgiev\Doctrine\DBAL\Types\Exceptions\InvalidHalfvecForPHPException;
@@ -19,65 +18,27 @@ use MartinGeorgiev\Doctrine\DBAL\Types\Exceptions\InvalidHalfvecForPHPException;
  *
  * @author Martin Georgiev <martin.georgiev@gmail.com>
  */
-class Halfvec extends BaseType
+class Halfvec extends BaseVector
 {
     protected const TYPE_NAME = Type::HALFVEC;
 
-    public function convertToDatabaseValue($value, AbstractPlatform $platform): ?string
+    protected function throwInvalidTypeForDatabase(mixed $value): never
     {
-        if ($value === null) {
-            return null;
-        }
-
-        if (!\is_array($value)) {
-            throw InvalidHalfvecForDatabaseException::forInvalidType($value);
-        }
-
-        if (!\array_is_list($value)) {
-            throw InvalidHalfvecForDatabaseException::forInvalidType($value);
-        }
-
-        $stringItems = [];
-        foreach ($value as $item) {
-            if (!\is_float($item) && !\is_int($item)) {
-                throw InvalidHalfvecForDatabaseException::forInvalidItemType($item);
-            }
-
-            $stringItems[] = (string) $item;
-        }
-
-        return '['.\implode(',', $stringItems).']';
+        throw InvalidHalfvecForDatabaseException::forInvalidType($value);
     }
 
-    public function convertToPHPValue($value, AbstractPlatform $platform): ?array
+    protected function throwInvalidItemTypeForDatabase(mixed $value): never
     {
-        if ($value === null) {
-            return null;
-        }
+        throw InvalidHalfvecForDatabaseException::forInvalidItemType($value);
+    }
 
-        if (!\is_string($value)) {
-            throw InvalidHalfvecForPHPException::forInvalidType($value);
-        }
+    protected function throwInvalidTypeForPHP(mixed $value): never
+    {
+        throw InvalidHalfvecForPHPException::forInvalidType($value);
+    }
 
-        if (\strlen($value) < 2 || $value[0] !== '[' || $value[\strlen($value) - 1] !== ']') {
-            throw InvalidHalfvecForPHPException::forInvalidFormat($value);
-        }
-
-        $trimmed = \substr($value, 1, -1);
-        if ($trimmed === '') {
-            return [];
-        }
-
-        $parts = \explode(',', $trimmed);
-        $result = [];
-        foreach ($parts as $part) {
-            if (!\is_numeric($part)) {
-                throw InvalidHalfvecForPHPException::forInvalidFormat($value);
-            }
-
-            $result[] = (float) $part;
-        }
-
-        return $result;
+    protected function throwInvalidFormatForPHP(mixed $value): never
+    {
+        throw InvalidHalfvecForPHPException::forInvalidFormat($value);
     }
 }
--- tests/Integration/MartinGeorgiev/Doctrine/DBAL/Types/VectorTypeTestCase.php
+++ tests/Integration/MartinGeorgiev/Doctrine/DBAL/Types/VectorTypeTestCase.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Tests\Integration\MartinGeorgiev\Doctrine\DBAL\Types;
 
+use PHPUnit\Framework\Attributes\Test;
+
 abstract class VectorTypeTestCase extends TestCase
 {
     protected function setUp(): void
@@ -12,7 +14,7 @@ abstract class VectorTypeTestCase extends TestCase
         $this->ensureVectorExtension();
     }
 
-    #[\PHPUnit\Framework\Attributes\Test]
+    #[Test]
     public function can_handle_null_values(): void
     {
         $this->runDbalBindingRoundTrip($this->getTypeName(), $this->getPostgresTypeName(), null);
