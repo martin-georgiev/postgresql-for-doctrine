8ce01bd9b81d1557fde004586fad31d01a180d43
Martin Georgiev <martin.georgiev@gmail.com>
2026-05-27 01:36:09 +0300

    no message

--- tests/Integration/MartinGeorgiev/Doctrine/ORM/Query/AST/Functions/Ltree/TestCase.php
+++ tests/Integration/MartinGeorgiev/Doctrine/ORM/Query/AST/Functions/Ltree/TestCase.php
@@ -11,6 +11,9 @@ abstract class TestCase extends BaseTestCase
     protected function setUp(): void
     {
         parent::setUp();
+
+        $this->ensurePostgresExtensionInSchema('ltree');
+
         $this->createTestTableForLtreeFixture();
         $this->insertTestDataForLtreeFixture();
     }
