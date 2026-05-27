135228bb6d7cf90ae89686cf55a674dbe3308015
Martin Georgiev <martin.georgiev@gmail.com>
2026-05-27 22:12:40 +0300

    test: move XmlConcatTest to XmlTestCase and add entity property test

--- tests/Integration/MartinGeorgiev/Doctrine/ORM/Query/AST/Functions/XmlConcatTest.php
+++ tests/Integration/MartinGeorgiev/Doctrine/ORM/Query/AST/Functions/XmlConcatTest.php
@@ -7,7 +7,7 @@ namespace Tests\Integration\MartinGeorgiev\Doctrine\ORM\Query\AST\Functions;
 use MartinGeorgiev\Doctrine\ORM\Query\AST\Functions\XmlConcat;
 use PHPUnit\Framework\Attributes\Test;
 
-final class XmlConcatTest extends TextTestCase
+final class XmlConcatTest extends XmlTestCase
 {
     protected function getStringFunctions(): array
     {
@@ -20,7 +20,7 @@ final class XmlConcatTest extends TextTestCase
     public function concatenates_two_xml_values(): void
     {
         $dql = "SELECT XMLCONCAT('<a>1</a>', '<b>2</b>') as result
-                FROM Fixtures\\MartinGeorgiev\\Doctrine\\Entity\\ContainsTexts t
+                FROM Fixtures\\MartinGeorgiev\\Doctrine\\Entity\\ContainsXml t
                 WHERE t.id = 1";
 
         $result = $this->executeDqlQuery($dql);
@@ -32,11 +32,23 @@ final class XmlConcatTest extends TextTestCase
     public function concatenates_three_xml_values(): void
     {
         $dql = "SELECT XMLCONCAT('<a>hello</a>', '<b>world</b>', '<c>!</c>') as result
-                FROM Fixtures\\MartinGeorgiev\\Doctrine\\Entity\\ContainsTexts t
+                FROM Fixtures\\MartinGeorgiev\\Doctrine\\Entity\\ContainsXml t
                 WHERE t.id = 1";
 
         $result = $this->executeDqlQuery($dql);
         $this->assertIsString($result[0]['result']);
         $this->assertSame('<a>hello</a><b>world</b><c>!</c>', $result[0]['result']);
+    }
+
+    #[Test]
+    public function concatenates_xml_values_with_entity_property(): void
+    {
+        $dql = "SELECT XMLCONCAT('<prefix/>', t.content) as result
+                FROM Fixtures\\MartinGeorgiev\\Doctrine\\Entity\\ContainsXml t
+                WHERE t.id = 1";
+
+        $result = $this->executeDqlQuery($dql);
+        $this->assertIsString($result[0]['result']);
+        $this->assertSame('<prefix/><root><item>foo</item><item>bar</item></root>', $result[0]['result']);
     }
 }
