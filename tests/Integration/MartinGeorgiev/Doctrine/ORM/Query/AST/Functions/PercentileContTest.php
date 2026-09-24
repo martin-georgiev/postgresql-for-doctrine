da79d693d095beb9a183d29bc266c79edff5e6a8
Martin Georgiev <martin.georgiev@gmail.com>
2026-09-25 02:27:48 +0300

    test: pair the percentile literal tests with entity field ones

--- tests/Integration/MartinGeorgiev/Doctrine/ORM/Query/AST/Functions/PercentileContTest.php
+++ tests/Integration/MartinGeorgiev/Doctrine/ORM/Query/AST/Functions/PercentileContTest.php
@@ -17,9 +17,17 @@ final class PercentileContTest extends NumericTestCase
     }
 
     #[Test]
-    public function returns_the_continuous_percentile_from_an_entity_field(): void
+    public function returns_the_continuous_percentile_from_a_literal(): void
     {
         $dql = 'SELECT PERCENTILE_CONT(0.5 WITHIN GROUP ORDER BY n.decimal1) as result FROM Fixtures\MartinGeorgiev\Doctrine\Entity\ContainsNumerics n WHERE n.id = 1';
+        $result = $this->executeDqlQuery($dql);
+        $this->assertEquals(10.5, $result[0]['result']);
+    }
+
+    #[Test]
+    public function returns_the_continuous_percentile_from_entity_fields(): void
+    {
+        $dql = 'SELECT PERCENTILE_CONT((n.decimal1 / n.decimal2) WITHIN GROUP ORDER BY n.decimal1) as result FROM Fixtures\MartinGeorgiev\Doctrine\Entity\ContainsNumerics n WHERE n.id = 1';
         $result = $this->executeDqlQuery($dql);
         $this->assertEquals(10.5, $result[0]['result']);
     }
--- tests/Integration/MartinGeorgiev/Doctrine/ORM/Query/AST/Functions/PercentileDiscTest.php
+++ tests/Integration/MartinGeorgiev/Doctrine/ORM/Query/AST/Functions/PercentileDiscTest.php
@@ -17,9 +17,17 @@ final class PercentileDiscTest extends NumericTestCase
     }
 
     #[Test]
-    public function returns_the_discrete_percentile_from_an_entity_field(): void
+    public function returns_the_discrete_percentile_from_a_literal(): void
     {
         $dql = 'SELECT PERCENTILE_DISC(0.5 WITHIN GROUP ORDER BY n.decimal1) as result FROM Fixtures\MartinGeorgiev\Doctrine\Entity\ContainsNumerics n WHERE n.id = 1';
+        $result = $this->executeDqlQuery($dql);
+        $this->assertSame('10.5', $result[0]['result']);
+    }
+
+    #[Test]
+    public function returns_the_discrete_percentile_from_entity_fields(): void
+    {
+        $dql = 'SELECT PERCENTILE_DISC((n.decimal1 / n.decimal2) WITHIN GROUP ORDER BY n.decimal1) as result FROM Fixtures\MartinGeorgiev\Doctrine\Entity\ContainsNumerics n WHERE n.id = 1';
         $result = $this->executeDqlQuery($dql);
         $this->assertSame('10.5', $result[0]['result']);
     }
