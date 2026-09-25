9e596c8a7a149cc6f936f59d82c066e2bf37bd43
Martin Georgiev <martin.georgiev@gmail.com>
2026-09-25 12:50:00 +0300

    test: build the object token without the Token class Lexer 1.x lacks

--- tests/Unit/MartinGeorgiev/Utils/DoctrineLexerTest.php
+++ tests/Unit/MartinGeorgiev/Utils/DoctrineLexerTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Tests\Unit\MartinGeorgiev\Utils;
 
-use Doctrine\Common\Lexer\Token;
 use MartinGeorgiev\Utils\DoctrineLexer;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Test;
@@ -28,9 +27,23 @@ final class DoctrineLexerTest extends TestCase
             'value of an array token' => [['value' => 'ROWS', 'type' => 100, 'position' => 0], 'value', 'ROWS'],
             'type of an array token' => [['value' => 'ROWS', 'type' => 100, 'position' => 0], 'type', 100],
             'missing field of an array token' => [['value' => 'ROWS'], 'type', null],
-            'value of an object token' => [new Token('ROWS', 100, 0), 'value', 'ROWS'],
-            'missing field of an object token' => [new Token('ROWS', 100, 0), 'length', null],
+            'value of an object token' => [self::createObjectToken(), 'value', 'ROWS'],
+            'missing field of an object token' => [self::createObjectToken(), 'length', null],
             'no token' => [null, 'value', null],
         ];
+    }
+
+    /**
+     * Shaped like Lexer 2.0+'s Token, which Lexer 1.x does not ship.
+     */
+    private static function createObjectToken(): object
+    {
+        return new class {
+            public string $value = 'ROWS';
+
+            public int $type = 100;
+
+            public int $position = 0;
+        };
     }
 }
